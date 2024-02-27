68bb2da12fcc32e98e0039afa3d8e6c20bb0cecc
Tatiane Ramos <158481113+hellotatiramos@users.noreply.github.com>
2024-02-27 14:22:02 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
-function Home(){
-    return <h1>Amei meu presente!!! Estou amando o curso.dev. Love yoouu! 💖 </h1>
+function Home() {
+  return (
+    <h1>Amei meu presente!!! Estou amando o curso.dev. Love yoouu! 💖 </h1>
+  );
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("teste2");
 }
 
 export default Home;
